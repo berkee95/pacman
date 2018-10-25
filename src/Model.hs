5e749557c23fa7395db0bf9f11ee748b5d98d9d3
Berke Kokhan <berke_gs_95@hotmail.com>
2018-10-25 17:01:05 +0200

    pacman

--- src/Model.hs
+++ src/Model.hs
@@ -12,7 +12,23 @@ nO_SECS_BETWEEN_CYCLES = 5
 data GameState = GameState {
                    infoToShow  :: InfoToShow
                  , elapsedTime :: Float
+                 , pacman      :: PacMan
                  }
 
+data Direction = Up|Left|Right|Down
+
+data KillAble = Killer | GetKilled
+
+data PacMan = PacMan
+              { location  :: (Int,Int)
+               ,direction :: Direction
+               ,velocity  :: (Int,Int)
+               ,timer     :: Int 
+               ,killable  :: KillAble
+               ,lifes     :: Int
+              }
+
+
+
 initialState :: GameState
-initialState = GameState ShowNothing 0
+initialState = GameState ShowNothing 0 pacman {location = (0,0), direction = Up, velocity = (0,1), timer = 0, killable = GetKilled, lifes = 3}
